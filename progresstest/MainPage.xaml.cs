e904c9808c43cdaa8ab1eb94f8fa219a1a5b010e
NGU ZHENG WEI <bcs21090007@student.uts.edu.my>
2023-12-01 16:14:16 +0800

    Add project files.

---
+++ progresstest/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace progresstest
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnReadMoreClicked(object sender, EventArgs e)
+        {
+            DescriptionLabel.IsVisible = !DescriptionLabel.IsVisible;
+        }
+
+    }
+}
